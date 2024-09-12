d37b397da36a0a6afb9caa8727962c17e99e5e05
agpaknazli <agpaknazli19@gmail.com>
2024-09-12 23:24:59 +0200

    setdate-setname

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
@@ -11,6 +12,8 @@ function AddModal({handleClose, show,drName}) {
 
 //   const handleClose = () => setShow(false);
 //   const handleShow = () => setShow(true);
+const [name,Setname] =useState("")
+const [date, Setdate] =useState("")
 
   return (
     <>
