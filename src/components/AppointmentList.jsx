8979e2fec463339a2faa1a1d8996ba031d31df9e
agpaknazli <agpaknazli19@gmail.com>
2024-09-14 16:05:18 +0200

    JSON

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -9,10 +9,10 @@ const AppointmentList = ({appointments,handleDelete,handleDoubleClick}) => {
 <Container className='p-2'>
 
 <h3 className='display-6 mb-2' style={{color:"rgb(166,18,189)"}}>Appointment List</h3>
-{appointments.length === 0 && (
+{appointments?.length === 0 && (
         <img src="./img/appointment.jpg" width="70%" alt="No appointments" />
       )}
-{appointments.map(({id,patient,consulted,doctor,day})=>(
+{appointments?.map(({id,patient,consulted,doctor,day})=>(
   <div 
   key={id}
   className={consulted ? "appointments consulted" :'appointments'} 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,38 +3,40 @@ import AppointmentList from '../components/AppointmentList';
 import Doctors from '../components/Doctors';
 import {appointmentData} from '../helpers/data';
 const Home = () => {
-const[appointments,setAppointments]=useState(appointmentData);
-const handleAdd=(newAppointment)=>{
-  setAppointments([...appointments,newAppointment])
-};
-const handleDelete=(id)=>{
-  const filteredList= appointments.filter((item)=>item.id !==id);
-  setAppointments(filteredList);
-};
-const handleDoubleClick=(id)=>{
-const updatedList=appointments.map((app)=>
-app.id===id ? {...app,consulted: !app.consulted} : app);
-setAppointments(updatedList);
-
-
-}
+  const [appointments, setAppointments] = useState(
+    JSON.parse(localStorage.getItem("list")) || []
+  );
+  const handleAdd = (newAppointment) => {
+    setAppointments([...appointments, newAppointment]);
+    localStorage.setItem(
+      "list",
+      JSON.stringify([...appointments, newAppointment])
+    );
+  };
+  const handleDelete = (id) => {
+    const filteredList = appointments.filter((item) => item.id !== id);
+    setAppointments(filteredList);
+    localStorage.setItem("list", JSON.stringify(filteredList));
+  };
+  const handleDoubleClick = (id) => {
+    const updatedList = appointments.map((app) =>
+      app.id === id ? { ...app, consulted: !app.consulted } : app
+    );
+    setAppointments(updatedList);
+    localStorage.setItem("list", JSON.stringify(updatedList));
+  };
 
   return (
-   <main className='text-center mt-2'>
-
-   <h1 className='display-5 text-danger'>KİEL HOSPİTAL</h1>
-
-<Doctors handleAdd={handleAdd}/>
-
-<AppointmentList appointments={appointments} handleDelete={handleDelete} handleDoubleClick={handleDoubleClick}/>
-
-
-
-
-
- </main>
-
-  )
-}
+    <main className="text-center mt-2">
+      <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
+      <Doctors handleAdd={handleAdd} />
+      <AppointmentList
+        appointments={appointments}
+        handleDelete={handleDelete}
+        handleDoubleClick={handleDoubleClick}
+      />
+    </main>
+  );
+};
 
 export default Home
