146d18afca8ffee078ecbcef5384581ccde51079
agpaknazli <agpaknazli19@gmail.com>
2024-09-14 12:22:46 +0200

    home.jsx

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { MdDelete } from "react-icons/md";
+
 const AppointmentList = ({appointments}) => {
   return (
 <Container className='p-2'>
 
 <h3 className='display-6 mb-2' style={{color:"rbg(166,18,189)"}}>Appointment List</h3>
 
-{appointments.map({id,patient,consulted,doctor,day})=>(
-  <div>
-    <Row>
+{appointments.map(({id,patient,consulted,doctor,day})=>(
+  <div className={consulted ? "appointments consulted" :'appointments'}>
+    <Row className='justify-content-between alings-items-center'>
+      <Col>
+      <h4>{patient}</h4>
+      <h5>{doctor}</h5>
+      </Col>
+      <Col>
+      <h5>Date: {new Date(day).toLocaleTimeString('de-DE')}</h5>
+      <h4>
+        Time: {new Date(day).toLocaleDateString('de-DE')}
+      </h4>
+      </Col>
       
+      <Col className='text-end'>
+      <MdDelete className='text-danger display-3 type="button"'/>
+      </Col>
     </Row>
   </div>
-)}
+))};
 
 
 
